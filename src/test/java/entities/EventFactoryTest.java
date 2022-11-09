d9560e095438a5536c5b31cedb0e2e0994c17d25
missionStrawberry <annam7245@sd61learn.ca>
2022-11-09 15:57:33 -0500

    finished testing for EventFactory

--- src/test/java/entities/EventFactoryTest.java
+++ src/test/java/entities/EventFactoryTest.java
@@ -2,11 +2,18 @@ package entities;
 
 import org.junit.jupiter.api.Test;
 
+import java.sql.Time;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EventFactoryTest {
 
     @Test
-    void createEvent() {
+    void createEventTest() {
+        EventFactory factory = new EventFactory();
+        Event festival = factory.createEvent("festival", new Time(5000000), new Time(60000000));
+        assertEquals("festival", festival.getTitle());
+        assertEquals(5000000, festival.getStartTime().getTime());
+        assertEquals(60000000, festival.getEndTime().getTime());
     }
 }
