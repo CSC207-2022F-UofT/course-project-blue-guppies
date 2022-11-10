b0a37b947455498df0530eb96c9352ce68c176d2
Fard-Faru <fardin2273faruk@gmail.com>
2022-11-10 02:28:14 -0500

    Implemented Task Entity with Factory with Documentation and Unit tests

--- src/main/java/entities/Task.java
+++ src/main/java/entities/Task.java
@@ -1,4 +1,28 @@
+/**
+ * Task class.
+ * Author: Fardin Faruk
+ * Modified by: NA
+ * Date created: Nov 10, 2022
+ * Last Modified: NA
+ */
 package entities;
-
 public class Task {
+    private String title;
+
+    protected Task(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return this.title;
+    }
 }
--- src/main/java/entities/TaskFactory.java
+++ src/main/java/entities/TaskFactory.java
@@ -1,4 +1,20 @@
+/**
+ * TaskFactory creates a new Task Object.
+ * Author: Fardin Faruk
+ * Modified by: NA
+ * Date created: Nov 10, 2022
+ * Last Modified: NA
+ */
 package entities;
 
 public class TaskFactory {
+    /**
+     * Creates a new Task
+     * Precondition: title is a non-empty string
+     * @param title title of the task
+     * @return a task create with the given inputs
+     */
+    public Task creatTask(String title){
+        return new Task(title);
+    }
 }
