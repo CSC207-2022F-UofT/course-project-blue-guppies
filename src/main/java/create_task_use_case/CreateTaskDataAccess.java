5389e6b66c74a7a5770a4256c788f274216f2e30
Fard-Faru <fardin2273faruk@gmail.com>
2022-11-19 15:19:38 -0500

    Update Base of Branch

--- src/main/java/create_task_use_case/CreateTaskDataAccess.java
+++ src/main/java/create_task_use_case/CreateTaskDataAccess.java
@@ -1,2 +1,8 @@
-package create_task_use_case;public class CreateTaskDataAccess {
+package create_task_use_case;
+
+import data_access.WeekDataAccess;
+
+public class CreateTaskDataAccess extends WeekDataAccess {
+
+
 }
