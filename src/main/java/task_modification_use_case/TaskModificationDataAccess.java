7d22aac031d2b5fc687ab2c04cb557ed1b260894
Raghav Arora <ragaro214@gmail.com>
2022-11-19 16:35:08 -0500

    finished business logic for Data Access layer, revised test cases, interactor test case to be fixed

--- src/main/java/task_modification_use_case/TaskModificationDataAccess.java
+++ src/main/java/task_modification_use_case/TaskModificationDataAccess.java
@@ -7,17 +7,45 @@
  */
 package task_modification_use_case;
 
-public class TaskModificationDataAccess implements TaskModificationDsGateway {
+import data_access.DataAccessDay;
+import data_access.DataAccessTask;
+import data_access.WeekDataAccess;
+import java.util.HashMap;
+
+public class TaskModificationDataAccess extends WeekDataAccess implements TaskModificationDsGateway {
 
     public TaskModificationDataAccess() {}
 
+    /**
+     * Check whether a task with the given title already exists for the pertinent day.
+     * Precondition: title is a non-empty string
+     * @param title the title the user wants to change the name of a selected task to
+     * @param dayID the ID of the day instance the selected task belongs to
+     * @return whether there already exists a task in the given day with the same name
+     */
     @Override
     public boolean taskExistsByTitle(String title, int dayID) {
-        return false;
+        DataAccessDay day = this.getDays().get(dayID);
+        return day.getTasks().containsKey(title);
     }
 
     @Override
     public void save(TaskModificationDsInputData dsInputData) {
+        String title = dsInputData.getTitle();
+        String newTitle = dsInputData.getNewTitle();
+        int dayId = dsInputData.getDayID();
 
+        /* Remove the association of the task to be modified to key title, and instead
+        associate it with newTitle */
+        DataAccessDay day = this.getDays().get(dayId);
+        HashMap<String, DataAccessTask> tasks = day.getTasks();
+        DataAccessTask modifiedTask = tasks.remove(title);
+        modifiedTask.setTitle(newTitle);
+        tasks.put(newTitle, modifiedTask);
+
+        // update the tasks for the given day
+        day.setTasks(tasks);
+        this.days.set(dayId, day);
+        this.Save();
     }
 }
--- src/test/java/task_modification_use_case/TaskModificationInteractorTest.java
+++ src/test/java/task_modification_use_case/TaskModificationInteractorTest.java
@@ -1,15 +1,32 @@
 package task_modification_use_case;
 
+import data_access.DataAccessDay;
+import data_access.DataAccessEvent;
+import data_access.DataAccessTask;
+import data_access.WeekDataAccess;
+
+import java.util.HashMap;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class TaskModificationInteractorTest {
+class TaskModificationInteractorTest extends WeekDataAccess {
     private final static TaskModificationInputData inputData = new TaskModificationInputData(
             0,"New Sample Task", "Sample Task"
     );
     private final static TaskModificationPresenter outputBoundary = new TaskModificationPresenter();
     private final static TaskModificationDataAccess dsGateway = new TaskModificationDataAccess();
+    private final static WeekDataAccess week = new WeekDataAccess();
+
+    @BeforeAll
+    public static void setUpWeekDays() {
+        HashMap<String, DataAccessTask> tasks = new HashMap<>();
+        HashMap<String, DataAccessEvent > events = new HashMap<>();
+    }
 
     @Test
     public void testModifyTask(){
