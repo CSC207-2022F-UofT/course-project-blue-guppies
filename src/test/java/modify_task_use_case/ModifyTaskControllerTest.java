3983397844dffda5e6f6d97a0a4dc851c58fb00b
Raghav Arora <ragaro214@gmail.com>
2022-11-21 11:28:45 -0500

    check controller test cases

--- src/test/java/modify_task_use_case/ModifyTaskControllerTest.java
+++ src/test/java/modify_task_use_case/ModifyTaskControllerTest.java
@@ -27,8 +27,7 @@ public class ModifyTaskControllerTest extends WeekDataAccess {
         this.days.set(0, day);
 
         ModifyTaskInteractor inputBoundary = new ModifyTaskInteractor(
-                outputBoundary,
-                dsGateway
+                outputBoundary, dsGateway
         );
         ModifyTaskController controller = new ModifyTaskController(inputBoundary);
         ModifyTaskOutputData outputData = controller.modifyTask(
