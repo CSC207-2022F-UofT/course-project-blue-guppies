7c8556edf6a1457b69330ea8cdc6fbb7ff8581b5
Fard-Faru <fardin2273faruk@gmail.com>
2022-12-03 02:11:14 -0500

    Added missing test cases

--- src/test/java/create_task_use_case/CreateTaskOutputDataTest.java
+++ src/test/java/create_task_use_case/CreateTaskOutputDataTest.java
@@ -18,10 +18,25 @@ public class CreateTaskOutputDataTest {
         assertEquals(outputData.getDayIndex(), 1);
     }
     @Test
-    public void testGet(){
+    public void testIsSuccessfullyCreated(){
+        CreateTaskOutputData outputData = new CreateTaskOutputData("Feed Dogs", 0,
+                "");
+        outputData.setSuccessfullyCreated(true);
+        assertTrue(outputData.isSuccessfullyCreated());
+    }
+    @Test
+    public void testGetErrorMessage(){
         CreateTaskOutputData outputData = new CreateTaskOutputData("Meal Prep", 1,
                 "There already exists a task with name 'Study' on Monday");
         assertEquals("There already exists a task with name 'Study' on Monday",
+                outputData.getErrorMessage());
+    }
+    @Test
+    public void testSetErrorMessage(){
+        CreateTaskOutputData outputData = new CreateTaskOutputData("Meal Prep", 1,
+                "There already exists a task with name 'Study' on Monday");
+        outputData.setErrorMessage("There already exists a task with name 'Study' on Sunday");
+        assertEquals("There already exists a task with name 'Study' on Sunday",
                 outputData.getErrorMessage());
     }
 }
