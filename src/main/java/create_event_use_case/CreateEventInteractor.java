5969035c75aae9f1dc7ca55809ae1e7055e5b798
missionStrawberry <annam7245@sd61learn.ca>
2022-12-03 17:23:34 -0500

    added more checks to the interactor

--- src/main/java/create_event_use_case/CreateEventInteractor.java
+++ src/main/java/create_event_use_case/CreateEventInteractor.java
@@ -3,12 +3,14 @@ package create_event_use_case;
 import entities.EventFactory;
 import entities.Event;
 
+import java.time.LocalTime;
+
 public class CreateEventInteractor implements CreateEventInputBoundary {
     private final CreateEventDsGateway dsGateway;
     private final CreateEventOutputBoundary outputBoundary;
     private final EventFactory factory;
 
-    final private String[] DAYSOFWEEK = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+    private final static String[] DAYS_OF_WEEK = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
     public CreateEventInteractor(CreateEventDsGateway dsGateway,
                                  CreateEventOutputBoundary outputBoundary,
@@ -20,21 +22,25 @@ public class CreateEventInteractor implements CreateEventInputBoundary {
 
     @Override
     public CreateEventOutputData create(CreateEventInputData inputData) {
-        CreateEventOutputData outputData;
+        CreateEventOutputData outputData = new CreateEventOutputData();;
         if (!(inputData.getStartTime().matches("[01][0-9]:[0-5][0-9]|2[0-3]:[0-5][0-9]"))) {
-            outputData = new CraateEventOutputData();
-
-        }
-
-        if (dsGateway.eventExistsByTitle(inputData.getTitle(), inputData.getDayIndex())) {
+            return outputBoundary.prepareFailView(outputData, "Start time is not a valid time. \nEx: 21:39");
+        } else if (!(inputData.getEndTime().matches("[01][0-9]:[0-5][0-9]|2[0-3]:[0-5][0-9]"))) {
+            return outputBoundary.prepareFailView(outputData, "End time is not a valid time. \nEx: 21:39");
+        } else if (dsGateway.eventExistsByTitle(inputData.getTitle(), inputData.getDayIndex())) {
             return outputBoundary.prepareFailView(outputData,
                     "Event with name '" + inputData.getTitle() + "' already exists on " +
-                            DAYSOFWEEK[inputData.getDayIndex()]
-                    );
+                            DAYS_OF_WEEK[inputData.getDayIndex()]
+            );
         }
-        Event event = factory.createEvent(
-                inputData.getTitle(), inputData.getStartTime(), inputData.getEndTime()
-        );
+        LocalTime startTime = LocalTime.parse(inputData.getStartTime());
+        LocalTime endTime = LocalTime.parse(inputData.getEndTime());
+
+        if (!startTime.isBefore(endTime)) {
+            return outputBoundary.prepareFailView(outputData, "Start time is not before end time.");
+        }
+
+        Event event = factory.createEvent(inputData.getTitle(), startTime, endTime);
 
         outputData = new CreateEventOutputData(
                 inputData.getTitle(), inputData.getStartTime(),
