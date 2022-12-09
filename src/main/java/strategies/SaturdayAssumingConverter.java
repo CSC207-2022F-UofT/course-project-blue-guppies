7d71caf5e12877f49f81e030b38b745343893fd1
DanielLivshits <livsdani829@gmail.com>
2022-12-08 21:49:56 -0500

    Added documentation to the Converter Classes.

--- src/main/java/strategies/SaturdayAssumingConverter.java
+++ src/main/java/strategies/SaturdayAssumingConverter.java
@@ -1,7 +1,17 @@
 package strategies;
 
+/**
+ * A DayToIndexConverter that converts the name of a day (capitalized) into an index corresponding to that day
+ * (specifically the way these days would be organized in a List assuming a week starts on Sunday).
+ * @author Daniel Livshits
+ */
 public class SaturdayAssumingConverter implements DayToIndexConverter{
 
+    /**
+     * Converts the name of a day into the index for that day in a List sorted in the order of a Sunday-Saturday week.
+     * @param dayName the name of the day
+     * @return the index corresponding to dayName
+     */
     public int convertDayNameToIndex(String dayName){
         switch (dayName) {
             case "Sunday":
